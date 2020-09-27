8d1753990d4c950e48fc49e239d6888c15cdb900
tavoGeek <gcnctz@gmail.com>
2020-09-26 20:25:55 -0500

    Configuracion de barra de navegacion con boton de cambio de idioma

--- src/components/BarraNavegacion.vue
+++ src/components/BarraNavegacion.vue
@@ -3,31 +3,17 @@
     <v-app-bar color="deep-purple accent-4" dense dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Prueba</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>Xiinbalkin</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-google-translate</v-icon>
+        ES
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
   </div>
 </template>
