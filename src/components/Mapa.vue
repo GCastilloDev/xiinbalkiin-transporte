bcf11e08aea763208096f9e59a437ecb9abe300a
tavoGeek <gcnctz@gmail.com>
2020-09-30 00:16:55 -0500

    :computer: Modulo de mapa

---
+++ src/components/Mapa.vue
@@ -0,0 +1,45 @@
+<template>
+  <div ref="contenedorMapa" class="mapa"></div>
+</template>
+
+<script>
+import L from "leaflet";
+
+export default {
+  name: "MapaComponent",
+  mounted() {
+    this.init();
+  },
+  data: () => ({
+    mapa: null,
+  }),
+  methods: {
+    async init() {
+      await this.pintarMapa();
+    },
+    pintarMapa() {
+      const contenedorMapa = this.$refs.contenedorMapa;
+
+      // instanciamos el mapa
+      this.mapa = L.map(contenedorMapa, {
+        center: [18.048627, -94.401241],
+        zoom: 18,
+      });
+
+
+      // le agregamos la capa de personalizacion
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 18,
+      }).addTo(this.mapa);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.mapa {
+  width: 100%;
+  height: 90vh;
+  z-index: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
+  <div class="home">    
     <BarraNavegacion />
+    <Mapa />
   </div>
 </template>
 
 <script>
 import BarraNavegacion from "../components/BarraNavegacion";
+import Mapa from "../components/Mapa";
 
 export default {
   name: "Home",
   components: {
     BarraNavegacion,
+    Mapa
   },
 };
 </script>
