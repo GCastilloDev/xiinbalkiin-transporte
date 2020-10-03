f22f65faa5f9de1f8dd9513ac35579f317400625
tavoGeek <gcnctz@gmail.com>
2020-10-03 01:56:51 -0500

    :computer: SECCION PRINCIPAL

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="home">    
+  <div>
     <BarraNavegacion />
     <Mapa />
+    <SeccionPrincipal />
   </div>
 </template>
 
 <script>
 import BarraNavegacion from "../components/BarraNavegacion";
 import Mapa from "../components/Mapa";
+import SeccionPrincipal from "../components/SeccionPrincipal";
 
 export default {
   name: "Home",
   components: {
     BarraNavegacion,
-    Mapa
+    Mapa,
+    SeccionPrincipal,
   },
 };
 </script>
